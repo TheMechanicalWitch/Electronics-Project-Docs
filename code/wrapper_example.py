18af885e194f45da00ac9332a48e2056af9a53e8
ase22003 <ase22003@student.mdu.se>
2026-06-26 10:35:52 +0200

    added example for can_wrapper.py

---
+++ code/wrapper_example.py
@@ -0,0 +1,19 @@
+import can_wrapper
+import time, random
+
+w = can_wrapper.CANWrapper()
+
+def displace_arm():
+    angles = w.get_angles()
+
+    for joint, angle in angles.items():
+        angles[joint] = angle + random.choice([10,-10])
+
+    w.command_angles(angles)
+
+def shake_arm():
+    for i in range(10):
+        displace_arm()
+        time.sleep(3)
+
+w.run_program(shake_arm)
