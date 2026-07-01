21bd8dba6b5267b173fa74d8581e8dc08185853e
Ailen Phoenix <cornelia.wahlman@gmail.com>
2026-07-01 12:17:35 +0200

    set up modularity for inv kinematics script

---
+++ code/kinematics/inverse.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import roboticstoolbox as rtb 
+from spatialmath import SE3
+
+from robotConfig import robot as ROBOT 
+
+
+
+
---
+++ code/kinematics/robotConfig.py
@@ -0,0 +1,28 @@
+import numpy as np 
+import roboticstoolbox as rtb 
+############################################################################################
+#robot configuration only edit this file                                                   #
+#                                                                                          #
+#rtb.DHRobot creates a dennavit hartenberg robot                                           #
+#joint order (1, 2, ... , n)                                                               #
+#attributes: (d(zDisplacement), a(xDisplacement), alpha(twistAngle), qlim(jointConstraint))#
+#                                                                                          #
+#rtb.RevoluteDh and rtb.PrismaticDH are the two joint declarations                         #
+############################################################################################
+
+
+#LIMB-HT25 defenition
+
+
+
+
+#constant config 
+
+
+################
+
+#declaration
+
+robot = rtb.DHRobot([
+    
+])
