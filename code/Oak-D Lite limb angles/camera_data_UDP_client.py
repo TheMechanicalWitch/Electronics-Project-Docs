d31024b320368a7ecf434e64669ae6f249c50db0
ase22003 <ase22003@student.mdu.se>
2026-07-02 19:30:34 +0200

    fix
    Increased buffer size for receiving data from 1024 to 4096 bytes.

--- code/Oak-D Lite limb angles/camera_data_UDP_client.py
+++ code/Oak-D Lite limb angles/camera_data_UDP_client.py
@@ -2,15 +2,15 @@ import socket
 import time
 import json
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-PORT = 12345
+CLIENT_IP = "0.0.0.0"
+CLIENT_PORT = 54321
 
-sock.bind(('127.0.0.1', PORT))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((CLIENT_IP, CLIENT_PORT))
 
 def get_points():
-    data, addr = sock.recvfrom(1024)
+    data, addr = sock.recvfrom(4096)
     return json.loads(data.decode())
 
 while True:
     print(get_points())
-    time.sleep(0.1)
