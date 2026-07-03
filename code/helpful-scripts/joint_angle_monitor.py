461ea4e209bb25cf699e151a895c3884a11e6e91
ase22003 <ase22003@student.mdu.se>
2026-07-03 16:45:45 +0200

    Improved angle display
    Added a nice_print function to format angle output.

--- code/helpful-scripts/joint_angle_monitor.py
+++ code/helpful-scripts/joint_angle_monitor.py
@@ -9,7 +9,12 @@ def monitor_angles():
     while True:
         angles = w.get_angles()
         os.system('clear')
-        print(angles)
+        nice_print(angles)
         time.sleep(UPDATE_DELAY)
+
+def nice_print(d:dict):
+    for key, val in sorted(d.items()):
+        if key in w.SOURCES:
+            print(f'{key} : {val:.2f}')
 
 w.run_program(monitor_angles)
