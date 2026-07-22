3bdb7af85c9509ba31aa36fb8703a5f358b04513
ase22003 <ase22003@student.mdu.se>
2026-07-22 16:22:34 +0200

    script now functional instead of placeholder

--- code/wrapper_example.py
+++ code/wrapper_example.py
@@ -1,19 +1,34 @@
+import socket
 import can_wrapper
-import time, random
+import time
+import json
+
+HOST = '0.0.0.0'
+PORT = 65432
 
 w = can_wrapper.CANWrapper()
 
-def displace_arm():
-    angles = w.get_angles()
+def start_server():
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            print(f"{HOST}:{PORT}...")
 
-    for joint, angle in angles.items():
-        angles[joint] = angle + random.choice([10,-10])
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected: {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
 
-    print(w.command_angles(angles, wait_until_complete=True, timeout=100.0, precision=10.0))
+                    message = data.decode('utf-8')
+                    angles = json.loads(message)
+                    w.command_angles(angles, wait_until_complete=False, timeout=10.0, precision=0.0)
+    except:
+        s.close()
+        print("closed server")
 
-def shake_arm():
-    for i in range(5):
-        displace_arm()
-        print("LOOP")
-
-w.run_program(shake_arm)
+if __name__ == "__main__":
+    w.run_program(start_server)
